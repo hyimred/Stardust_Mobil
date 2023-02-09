53bc408156ee82f9aef104ca34fae936957e1c06
hyimred <ptrk.csak@gmail.com>
2023-02-09 22:24:05 +0100

    animated gradient background

--- app/src/main/java/com/ptrkcsak/stardust_mobil/MainActivity.kt
+++ app/src/main/java/com/ptrkcsak/stardust_mobil/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.ptrkcsak.stardust_mobil
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //LOL
+        val layout_root = findViewById<View>(R.id.layout_root) as View
+        val animatedDrawable = layout_root.background as AnimationDrawable
+        animatedDrawable.setEnterFadeDuration(10)
+        animatedDrawable.setExitFadeDuration(5000)
+        animatedDrawable.start()
     }
 }
