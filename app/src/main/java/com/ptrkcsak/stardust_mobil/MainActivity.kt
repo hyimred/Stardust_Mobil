14284421fc13d27b83aa3951344a190059d6aa35
hyimred <ptrk.csak@gmail.com>
2023-02-09 15:10:24 +0100

    Start

---
+++ app/src/main/java/com/ptrkcsak/stardust_mobil/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ptrkcsak.stardust_mobil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
