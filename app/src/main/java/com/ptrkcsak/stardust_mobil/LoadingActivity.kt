a9d52356ca93d4dab2289a46836913bb1b033dc6
hyimred <ptrk.csak@gmail.com>
2023-02-10 13:12:56 +0100

    Loading ui

---
+++ app/src/main/java/com/ptrkcsak/stardust_mobil/LoadingActivity.kt
@@ -0,0 +1,36 @@
+package com.ptrkcsak.stardust_mobil
+
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.window.SplashScreen
+import android.os.Handler
+import android.view.View
+
+class LoadingActivity : AppCompatActivity() {
+
+    val SPLASH_TIME_OUT = 5000;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+        val layout_root = findViewById<View>(R.id.layout_root) as View
+        val animatedDrawable = layout_root.background as AnimationDrawable
+        animatedDrawable.setEnterFadeDuration(10)
+        animatedDrawable.setExitFadeDuration(3500)
+        animatedDrawable.start()
+        splashScreenAnimation()
+    }
+
+    fun splashScreenAnimation() {
+        Handler().postDelayed({
+                val intent =
+                        Intent(this@LoadingActivity, MainActivity::class.java)
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+        }, SPLASH_TIME_OUT.toLong())
+    }
+}
