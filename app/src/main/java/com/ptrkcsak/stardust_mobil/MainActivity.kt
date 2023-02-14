814f361e2cca3d7cdbe584f138cbece956284afc
hyimred <ptrk.csak@gmail.com>
2023-02-14 12:26:38 +0100

    menu and profile / settings activity

--- app/src/main/java/com/ptrkcsak/stardust_mobil/MainActivity.kt
+++ app/src/main/java/com/ptrkcsak/stardust_mobil/MainActivity.kt
@@ -1,22 +1,67 @@
 package com.ptrkcsak.stardust_mobil
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.MenuItem
 import android.view.View
+import android.widget.Button
+import android.widget.PopupMenu
 import android.widget.Toast
-
 class MainActivity : AppCompatActivity() {
+    val SPLASH_TIME_OUT = 1;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btn_menu = findViewById<Button>(R.id.btn_menu)
         val layout_root = findViewById<View>(R.id.layout_root_main) as View
         val animatedDrawable = layout_root.background as AnimationDrawable
+
         animatedDrawable.setEnterFadeDuration(10)
         animatedDrawable.setExitFadeDuration(5000)
         animatedDrawable.start()
 
+        btn_menu.setOnClickListener {
+            showPopup(btn_menu)
+        }
     }
+
+    private fun showPopup(view: View) {
+        val popup = PopupMenu(this, view)
+        popup.inflate(R.menu.menu)
+        popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item: MenuItem? ->
+            when (item!!.itemId) {
+                R.id.categories -> {
+
+                }
+                R.id.profile -> {
+                    Handler().postDelayed({
+                        val intent =
+                            Intent(this@MainActivity, ProfileActivity::class.java)
+                        startActivity(intent);
+                        finish();
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+                    }, SPLASH_TIME_OUT.toLong())
+                }
+                R.id.settings -> {
+                    Handler().postDelayed({
+                        val intent =
+                            Intent(this@MainActivity, SettingsActivity::class.java)
+                        startActivity(intent);
+                        finish();
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+                    }, SPLASH_TIME_OUT.toLong())
+                }
+            }
+            true
+        })
+        popup.show()
+    }
+
 
 }
---
+++ app/src/main/java/com/ptrkcsak/stardust_mobil/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.ptrkcsak.stardust_mobil
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val layout_root = findViewById<View>(R.id.layout_root_settings) as View
+        val animatedDrawable = layout_root.background as AnimationDrawable
+
+        animatedDrawable.setEnterFadeDuration(10)
+        animatedDrawable.setExitFadeDuration(5000)
+        animatedDrawable.start()
+    }
+}
