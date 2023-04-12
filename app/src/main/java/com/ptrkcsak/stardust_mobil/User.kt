65af80fc5cf547429adb2a39387992da27b93560
hyimred <ptrk.csak@gmail.com>
2023-04-12 18:06:22 +0200

    profile activity

--- app/src/main/java/com/ptrkcsak/stardust_mobil/User.kt
+++ app/src/main/java/com/ptrkcsak/stardust_mobil/User.kt
@@ -6,4 +6,5 @@ data class User(
     @SerializedName("id") val id: Int,
     @SerializedName("email") val email: String,
     @SerializedName("password") val password: String,
+    @SerializedName("reregistartionDate") val regregistartionDate: String,
 )
